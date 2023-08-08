7438e593118e3cb80954c848d9375fffca10f591
India Ugolin <iugolin@paul-f4Ar2s2.clusters.42paris.fr>
2023-08-08 16:03:09 +0200

    ex00

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,10 +6,31 @@
 /*   By: iugolin <iugolin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 19:29:43 by iugolin           #+#    #+#             */
-/*   Updated: 2023/07/26 19:43:56 by iugolin          ###   ########.fr       */
+/*   Updated: 2023/08/08 15:55:02 by iugolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int	main(int argc, char **argv)
+{
+    std::string empty_case_string = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    
+    if (argc < 2)
+    {
+        std::cout << empty_case_string << std::endl;
+        return (1);
+    }
+    else
+    {
+        for (int i = 1; argv[i]; i++)
+        {
+        for (int j = 0; argv[i][j]; j++)
+            std::cout << (char) toupper(argv[i][j]);
+        if (i < argc - 1)
+            std::cout << " ";
+        }
+    }
+    std::cout << std::endl;
+    return (0);
+}
